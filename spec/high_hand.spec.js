331a28ee5af710c03d037e70757baffdf8c96786
Lionel E. Ramos <ltrainpr@gmail.com>
2018-03-17 11:48:34 -0600

    Adds specs for full house, three of a kind, two pair, and one pair

--- spec/high_hand.spec.js
+++ spec/high_hand.spec.js
@@ -8,19 +8,44 @@ describe("HighHand", () => {
       expect(highHand(parameters)).toEqual({ hand: 'flush', value: 13, suit: 'heart' });
     });
 
-    it("straight", () => {
+    it("K high straight", () => {
       var parameters = ["Q_Heart J_Club", "K_Heart 10_Spade 9_Diamond"];
       expect(highHand(parameters)).toEqual({ hand: 'straight', value: 13, suit: '' });
     });
 
-    xit("straight flush", () => {
+    it("10 high straight", () => {
+      var parameters = ["6_Heart 7_Club", "8_Heart 10_Spade 9_Diamond"];
+      expect(highHand(parameters)).toEqual({ hand: 'straight', value: 10, suit: '' });
+    });
+
+    it("straight flush", () => {
       var parameters = ["Q_Club J_Club", "K_Club 10_Club 9_Club"];
       expect(highHand(parameters)).toEqual({ hand: 'straight flush', value: 13, suit: 'club' });
     });
 
     xit("four of a kind", () => {
-      var parameters = ["Q_Club Q_Spade", "Q_Heart Q_Diamond 9_Club"];
+      var parameters = ["Q_Club Q_Spade", "Q_Heart Q_Diamond A_Club"];
       expect(highHand(parameters)).toEqual({ hand: 'four of a kind', value: 12, suit: '' });
+    });
+
+    xit("full house", () => {
+      var parameters = ["Q_Club Q_Spade", "Q_Heart A_Diamond A_Club"];
+      expect(highHand(parameters)).toEqual({ hand: 'full house', value: 12, suit: '' });
+    });
+
+    xit("three of a kind", () => {
+      var parameters = ["Q_Club Q_Spade", "Q_Heart 5_Diamond A_Club"];
+      expect(highHand(parameters)).toEqual({ hand: 'three of a kind', value: 12, suit: '' });
+    });
+
+    xit("two pair", () => {
+      var parameters = ["Q_Club Q_Spade", "5_Heart 5_Diamond A_Club"];
+      expect(highHand(parameters)).toEqual({ hand: 'two pair', value: 12, suit: '', bottomPair: 5, kicker: 14 });
+    });
+
+    xit("one pair", () => {
+      var parameters = ["Q_Club J_Spade", "5_Heart 5_Diamond A_Club"];
+      expect(highHand(parameters)).toEqual({ hand: 'one pair', value: 5, suit: '', kicker: 14 });
     });
   });
 });
