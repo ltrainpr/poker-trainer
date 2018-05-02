2df78eb871f7f1f6fc989c2b363852be21f9d8e0
Lionel E. Ramos <ltrainpr@gmail.com>
2018-05-02 06:56:14 -0600

    WIP: First iteration at Game

---
+++ spec/game_spec.js
@@ -0,0 +1,11 @@
+var Game = require("../game");
+
+describe("Game", () => {
+  it("Creates Players", () => {
+    expect(Game().players.length).toBe(10)
+  });
+
+  it("Starts button at 4th index", () => {
+    expect(Game().button).toBe(4)
+  });
+});
