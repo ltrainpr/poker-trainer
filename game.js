c1e3b1999cc7a8f64ef9d22be406866d719efe53
Lionel E. Ramos <ltrainpr@gmail.com>
2018-05-15 08:17:45 -0600

    Refactor game as interface of functions to execute during game.

--- game.js
+++ game.js
@@ -1,7 +1,29 @@
 var _ = require('underscore');
 var Player = require("./player");
+var Dealer = require("./dealer");
+var Betting = require("./betting");
+
 
 var Game = function() {
+  var pot = 0;
+  var players = players || createPlayers();
+  var button = button || buttonIndex();
+  var dealer = dealer || Dealer();
+  var betting = betting || Betting();
+
+  return { players, button, dealer, betting };
+};
+
+function buttonIndex() {
+  function generateButtonIndex() { return _.random(9); }
+  function moveButton(button) { return button === 9 ? 0 : button + 1; }
+  function getButtonPosition() { return button; }
+  function underTheGunIndex(button) { return button + 3; }
+
+  return { generateButtonIndex, moveButton, getButtonPosition, underTheGunIndex };
+}
+
+function createPlayers() {
   var names =
   [
     "Amy",
@@ -16,21 +38,13 @@ var Game = function() {
     "Josh"
   ];
 
-  var players = players || createPlayers();
-
-  function createPlayers() {
-    var playersArray = [];
-    for (var i = 9; i >= 0; i--) {
-      playersArray.push(Player(names[i]));
-    }
-
-    return playersArray;
+  var playersArray = [];
+  for (var i = 9; i >= 0; i--) {
+    playersArray.push(Player(names[i]));
   }
 
-  return {
-    players:  players,
-    button:   _.random(9)
-  };
-};
+  return playersArray;
+}
+
 
 module.exports = Game;
